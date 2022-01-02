0953b371ee2a929575e256885dde7f78e845f1b2
Dennis Nieuwenhuijzen <dennis@cookies2.nl>
2022-01-02 22:18:41 +0100

    first

---
+++ dhcpListen.py
@@ -0,0 +1,9 @@
+from scapy.all import *
+
+captureInterface = "eth0"
+captureFilter = "port 67"
+
+def procdhcp(pkt):
+    pkt.show()
+
+pkts = sniff(iface=captureInterface,filter=captureFilter, count=0 ,prn=procdhcp, store=0)
