507f8c43c1dbea5237cf4d825548dc82873276bb
Dennis Nieuwenhuijzen <dennis@cookies2.nl>
2022-01-02 23:02:53 +0100

    update

--- dhcpListen.py
+++ dhcpListen.py
@@ -8,6 +8,6 @@ def procdhcp(pkt):
     print(pkt['Ethernet'].src)
     print(pkt['IP'].src)
     print(pkt['DHCP'].options)
-    print(re.sub(r".*param_req_list', \[(.*?)\].*",r'\1',pkt['DHCP'].options))
+    print(re.sub(r".*param_req_list', \[(.*?)\].*",r'\1',str(pkt['DHCP'].options)))
 
 pkts = sniff(iface=captureInterface,filter=captureFilter, count=0 ,prn=procdhcp, store=0)
